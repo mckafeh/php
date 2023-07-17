299b26153f0ea6366a2fb33419e4aff8e24e0765
mckafeh <43886116+mckafeh@users.noreply.github.com>
2023-07-17 19:08:21 +0200

    Simple php web site

---
+++ simple-php-website/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Commencvxvxxcct these lines to hide errors
+error_mnmnbreporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+
+init();
